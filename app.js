60e52dd4facafad6355af03dc96992155157a3fd
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-25 04:08:38 +0800

    Init

---
+++ app.js
@@ -0,0 +1,4 @@
+function count(num1, num2) {
+    return num1 + num2;
+}
+console.log(count(10, 20));
---
+++ app.ts
@@ -0,0 +1,5 @@
+function count(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(count(10, 20));
