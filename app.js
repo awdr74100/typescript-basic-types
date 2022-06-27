d65d545fafa2739ef21e30705c9064db0728b7ba
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-27 22:51:38 +0800

    Update progress

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-function count(num1, num2) {
-    return num1 + num2;
+function combine(input1, input2, resultConversion) {
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        return +input1 + +input2;
+    }
+    return input1.toString() + " " + input2.toString();
 }
-console.log(count(10, 20));
+var combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+var combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedAges);
+var combinedNames = combine("Yi-Ru", "Lan", "as-text");
+console.log(combinedNames);
---
+++ union-aliases.ts
@@ -0,0 +1,26 @@
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor
+) {
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    return +input1 + +input2;
+  }
+
+  return input1.toString() + " " + input2.toString();
+}
+
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedAges);
+
+const combinedNames = combine("Yi-Ru", "Lan", "as-text");
+console.log(combinedNames);
