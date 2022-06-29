df08cd6c304025c7d3ad3577d5f22f9abc884f79
Yi-Ru Lan <a78945612385238@gmail.com>
2022-06-29 22:11:29 +0800

    Update progress

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string") {
+  userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: " + num);
-    return null;
+function generateError(message, code) {
+  throw { message: message, code: code };
+  // while (true) {}
 }
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    var a = cb(result);
-    console.log(a);
-}
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result, ss) {
-    // console.log(result);
-    // console.log(dw);
-    return "dwcccc";
-});
+generateError("An error occurred!", 500);
